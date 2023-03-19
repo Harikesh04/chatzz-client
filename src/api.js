f5df6e822e3b27826e14b8eef0f0bb7754ee69c9
Harry <harikeshkrsingh20@gmail.com>
2023-03-19 23:43:09 +0530

    added link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
   //  const url = 'http://localhost:4000';
-   const url = 'https://chatzz-chat-app.herokuapp.com/';
+   const url = 'https://chatzz-backend.onrender.com/';
 
 const instance = axios.create({
   baseURL: url,
